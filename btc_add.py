438fe237db3784cc89a07ac80093ca650167a2c8
Clément Moussu <clement@bittorrent.com>
2012-07-25 16:17:05 -0700

    improve add by handling more errors and detecting input type

--- btc_add.py
+++ btc_add.py
@@ -6,28 +6,50 @@ import time
 from bencode import bencode, bdecode
 import utils
 import hashlib
-from btc import encoder, decoder, client
+import os
+from btc import encoder, decoder, client, error
 
 _description = 'add torrent to client'
 
 def main():
     parser = argparse.ArgumentParser()
-    group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument('-u', '--url', default=None)
-    group.add_argument('-f', '--file', default=None)
+    parser.add_argument('value')
+    group = parser.add_mutually_exclusive_group(required=False)
+    group.add_argument('-u', '--url', default=False, action='store_true')
+    group.add_argument('-f', '--file', default=False, action='store_true')
     args = parser.parse_args()
 
-    if args.url is not None:
-        client.add_torrent_url(args.url)
-        torrent = utils.get(args.url)
-    else:
-        client.add_torrent_file(args.file)
-        f = open(args.file, 'rb')
-        torrent = f.read()
-        f.close()
+    if not args.url and not args.file:
+        args.file = os.path.exists(args.value)
+        args.url = not args.file
+
+    if args.url:
+        args.value = utils.httpize(args.value)
+        try:
+            torrent = utils.get(args.value)
+        except:
+            error('invalid url: %s' % args.value)
+        client.add_torrent_url(args.value)
+    elif args.file:
+        if not os.path.exists(args.value):
+            error('no such file: %s' % args.value)
+        try:
+            f = open(args.value, 'rb')
+            torrent = f.read()
+            f.close()
+        except:
+            error('reading file: %s' % args.value)
+        client.add_torrent_file(args.value)
 
     added = None
-    h = hashlib.sha1(bencode(bdecode(torrent)['info'])).hexdigest().lower()
+
+    try:
+        decoded = bdecode(torrent)
+        encoded = bencode(decoded['info'])
+    except:
+        error('invalid torrent file')
+
+    h = hashlib.sha1(encoded).hexdigest().lower()
     while not added:
         l = client.list_torrents()
         for t in l:
