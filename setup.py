82dffaec3b69b5533ec67bb6005b8d9cea2f1b1a
Clement Moussu <clement@bittorrent.com>
2012-12-11 14:27:59 -0800

    make sure dependencies are installed for python < 2.7

--- setup.py
+++ setup.py
@@ -4,6 +4,12 @@ __author__ = 'Clement Moussu'
 __author_email__ = 'clement@bittorrent.com'
 
 from setuptools import setup, find_packages
+import sys
+
+requires = [ 'httplib2' ]
+
+if sys.version_info[0] == 2 and sys.version_info[1] < 7:
+    requires += ['argparse', 'ordereddict']
 
 setup(
     name = 'btc',
@@ -12,9 +18,7 @@ setup(
     author = __author__,
     author_email = __author_email__,
     description = 'command line tool for remote bittorent client control',
-    install_requires = [
-        'httplib2',
-    ],
+    install_requires = requires,
     entry_points = {
         'console_scripts': [
             'btc = btc.btc:main'
