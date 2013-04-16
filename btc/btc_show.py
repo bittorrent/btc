d080e00c3196da44094011c0517a1bc16fac7c18
Clement Moussu <clement@bittorrent.com>
2013-04-15 21:08:33 -0700

    btc show: accept multiple keys

--- btc/btc_show.py
+++ btc/btc_show.py
@@ -9,8 +9,8 @@ _description = 'show values and items'
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('key', metavar='KEY', nargs='?', default=None,
-                        help='key associated with value to be printed')
+    parser.add_argument('keys', metavar='KEY', nargs='*', default=None,
+                        help='keys associated with values to be printed')
     args = parser.parse_args()
 
     if sys.stdin.isatty():
@@ -25,23 +25,26 @@ def main():
     except ValueError:
         error('unexpected input: %s' % l)
 
-    if isinstance(l, list):
-        i = 0
-        for e in l:
-            if isinstance(e, dict):
-                if args.key is not None:
-                    try:
-                        print(e[args.key])
-                    except KeyError:
-                        error('key not found: {}'.format(args.key))
-                else:
-                    if i != 0:
-                        print('')
-                    items = list(ordered_dict(e).items())
-                    print(os.linesep.join(['{}: {}'.format(k, v) for k, v in items]))
-            else:
-                error('unexpected input: {}'.format(e))
-            i += 1
+    if not isinstance(l, list):
+        error('input must be a list')
+    elif args.keys and not all(isinstance(x, dict) for x in l):
+        error('list items must be dictionaries when specifying a key')
+
+    for i, e in enumerate(l):
+        if isinstance(e, dict):
+            keys = args.keys or ordered_dict(e).keys()
+            if i != 0 and len(keys) > 1:
+                print('')
+            for key in keys:
+                s = ''
+                if len(keys) > 1:
+                    s += '{}: '.format(key)
+                try:
+                    print('{}{}'.format(s, e[key]))
+                except KeyError:
+                    error('key not found: {}'.format(key))
+        else:
+            print(e)
 
 if __name__ == '__main__':
     main()
