67a76dd8f37eb2edc9f21348d9613d44b8e883be
Clement Moussu <clement@bittorrent.com>
2013-04-16 09:52:38 -0700

    btc show: add formatting options for priting values

--- btc/btc_show.py
+++ btc/btc_show.py
@@ -11,6 +11,10 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('keys', metavar='KEY', nargs='*', default=None,
                         help='keys associated with values to be printed')
+    parser.add_argument('-k', '--show-keys', action='store_true', default=False)
+    parser.add_argument('-s', '--separator', default=' ')
+    parser.add_argument('-S', '--entry-separator', default='')
+    parser.add_argument('-q', '--quote', default='')
     args = parser.parse_args()
 
     if sys.stdin.isatty():
@@ -25,6 +29,9 @@ def main():
     except ValueError:
         error('unexpected input: %s' % l)
 
+    if isinstance(l, dict):
+        l = [l]
+
     if not isinstance(l, list):
         error('input must be a list')
     elif args.keys and not all(isinstance(x, dict) for x in l):
@@ -34,13 +41,13 @@ def main():
         if isinstance(e, dict):
             keys = args.keys or ordered_dict(e).keys()
             if i != 0 and len(keys) > 1:
-                print('')
+                print(args.entry_separator)
             for key in keys:
                 s = ''
-                if len(keys) > 1:
-                    s += '{}: '.format(key)
+                if args.show_keys:
+                    s += '{0}{1}{0}{2}'.format(args.quote, key, args.separator)
                 try:
-                    print('{}{}'.format(s, e[key]))
+                    print('{0}{1}{2}{1}'.format(s, args.quote, e[key]))
                 except KeyError:
                     error('key not found: {}'.format(key))
         else:
